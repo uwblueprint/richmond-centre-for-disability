1c8a6944667c4d7b225b7c2ea294d5e944a28e34
Jeffrey Zhang <jeffrey346@gmail.com>
2021-06-13 15:12:18 -0400

    Small cleanup changes

--- prisma/index.ts
+++ prisma/index.ts
@@ -10,6 +10,8 @@ declare const global: CustomNodeJsGlobal;
 
 const prisma = global.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV === 'development') global.prisma = prisma;
+if (process.env.NODE_ENV === 'development') {
+  global.prisma = prisma;
+}
 
 export default prisma;
