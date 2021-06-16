f5bd0d640f45a7a313cb38a72c4523e896177032
Jeffrey Zhang <jeffrey346@gmail.com>
2021-06-16 18:54:32 -0400

    Change login form submit handling, add loading state
    Add comment to isLogginIn useState

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,4 +1,4 @@
-import { useState, KeyboardEvent } from 'react'; // React
+import { useState, SyntheticEvent } from 'react'; // React
 import { GetServerSideProps } from 'next'; // Get server side props
 import { getSession, signIn } from 'next-auth/client'; // Session management
 import { FormControl, FormLabel, Input, Button, Container, FormHelperText } from '@chakra-ui/react'; // Chakra UI
@@ -12,38 +12,41 @@ export default function Login() {
   // Store email in local storage
   const [, setLocalStorageEmail] = useLocalStorage('rcd-email-redirect', '');
 
+  // Loading state for log in button
+  const [isSigningIn, setIsSigningIn] = useState(false);
+
   /**
    * Process login using email input
    */
   const signInWithEmail = () => {
+    setIsSigningIn(true);
     setLocalStorageEmail(email);
     signIn('email', { email });
   };
 
-  /**
-   * Log in with email when the 'Enter' key is pressed
-   */
-  const handleKeyUp = (event: KeyboardEvent) => {
-    if (event.key === 'Enter') {
-      signInWithEmail();
-    }
+  const handleSubmit = (event: SyntheticEvent) => {
+    event.preventDefault();
+    signInWithEmail();
   };
 
   return (
     <Layout>
       <Container>
-        <FormControl>
-          <FormLabel>Login</FormLabel>
-          <Input
-            type="email"
-            placeholder="Your email"
-            value={email}
-            onChange={event => setEmail(event.target.value)}
-            onKeyUp={handleKeyUp}
-          />
-          <FormHelperText>Please enter an RCD email</FormHelperText>
-        </FormControl>
-        <Button onClick={signInWithEmail}>Log in</Button>
+        <form onSubmit={handleSubmit}>
+          <FormControl>
+            <FormLabel>Login</FormLabel>
+            <Input
+              type="email"
+              placeholder="Your email"
+              value={email}
+              onChange={event => setEmail(event.target.value)}
+            />
+            <FormHelperText>Please enter an RCD email</FormHelperText>
+          </FormControl>
+          <Button onClick={signInWithEmail} isLoading={isSigningIn} loadingText="Logging In">
+            Log in
+          </Button>
+        </form>
       </Container>
     </Layout>
   );
