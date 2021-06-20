888484af55f68f11d4e0db19f87141a12a0d68f5
EmilioMena <emilio.mena128@gmail.com>
2021-06-20 19:34:08 -0400

    update jsdoc for functions

--- lib/utils/format.ts
+++ lib/utils/format.ts
@@ -1,14 +1,16 @@
 /**
- * Format North American phone number.
- * Remove all non-numeric characters.
+ * Format North American phone number by removing all non-numeric characters.
+ * @param {string} phone phone number to be formatted
+ * @returns {string} formatted phone number
  */
 export const formatPhoneNumber = (phone: string): string => {
   return phone?.replace(/\D/g, '');
 };
 
 /**
- * Format Canadian postal code.
- * Remove all non-alphanumeric characters.
+ * Format Canadian postal code by removing all non-alphanumeric characters.
+ * @param {string} postalCode postal code to be formatted
+ * @returns {string} formatted postal code
  */
 export const formatPostalCode = (postalCode: string): string => {
   return postalCode?.replace(/[^A-Za-z\d]/g, '');
