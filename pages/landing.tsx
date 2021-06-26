92ddb2dc1e7e4472bd9fc23430043b3aa3c092e0
Angela Dietz <angiecdietz@gmail.com>
2021-06-26 12:01:28 -0400

    Add FAQ info

--- pages/landing.tsx
+++ pages/landing.tsx
@@ -2,7 +2,7 @@
 //import { useQuery } from '@apollo/client'; // GQL
 import { useTranslation } from 'next-i18next'; // Translation hook
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import { Text } from '@chakra-ui/react'; // Chakra UI
+import { Text, Divider, VStack, GridItem, Box, Grid } from '@chakra-ui/react'; // Chakra UI
 import Layout from '@components/applicant/Layout'; // Layout wrapper
 
 export default function Landing() {
@@ -11,8 +11,49 @@ export default function Landing() {
 
   return (
     <Layout>
-      <Text textStyle="display-xlarge">{t('landing')}</Text>
-      <Text textStyle="display-medium">hello</Text>
+      <GridItem colSpan={12} colStart={0}>
+        <VStack height="100%" width="100%" alignItems="left">
+          <Text as="h1" textStyle="display-xlarge">
+            {t('landing')}
+          </Text>
+          <Text as="h1" textStyle="display-medium">
+            hello
+          </Text>
+
+          <Divider borderColor="black.400" />
+          <br />
+          <Grid templateColumns="repeat(2, 1fr)" gap={6}>
+            <GridItem colSpan={2} colStart={0}>
+              <Text textStyle="display-large" textAlign="left">
+                Frequently Asked Questions
+              </Text>
+              <Text textStyle="display-small" textAlign="left">
+                Have any questions about our online services or our services? Look below to see some
+                of the most frequently asked questions.
+              </Text>
+            </GridItem>
+            <GridItem colSpan={1} colStart={3}>
+              <Box
+                width="385px"
+                borderWidth="1px"
+                borderRadius="12px"
+                bg="background.white"
+                borderColor="border.secondary"
+                padding="22px"
+              >
+                <Text textStyle="display-small" fontWeight="semibold" textAlign="left">
+                  Have more questions?
+                </Text>
+                <Text textStyle="body-regular" textAlign="left">
+                  Please contact RCD via phone at <br /> <b>604-232-2404</b> or via email at{' '}
+                  <b>parkingpermit@rcdrichmond.org</b>
+                </Text>
+              </Box>
+            </GridItem>
+            <br />
+          </Grid>
+        </VStack>
+      </GridItem>
     </Layout>
   );
 }
