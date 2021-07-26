01d6cbe2040697b36f6ad7d7c76cb6e096d92082
EmilioMena <emilio.mena128@gmail.com>
2021-07-25 23:22:20 -0400

    create permit holders table

--- pages/admin/permit-holders.tsx
+++ pages/admin/permit-holders.tsx
@@ -1,16 +1,260 @@
 import { GetServerSideProps } from 'next'; // Get server side props
 import { getSession } from 'next-auth/client'; // Session management
-import { Text, GridItem } from '@chakra-ui/react'; // Chakra UI
+import {
+  Box,
+  Flex,
+  Text,
+  GridItem,
+  Button,
+  Menu,
+  MenuButton,
+  InputGroup,
+  Input,
+  InputLeftElement,
+  useControllableState,
+  MenuList,
+  MenuItem,
+  Wrap,
+  Badge,
+} from '@chakra-ui/react'; // Chakra UI
+import { ChevronDownIcon, SearchIcon } from '@chakra-ui/icons'; // Chakra UI Icons
 import Layout from '@components/internal/Layout'; // Layout component
 import { Role } from '@lib/types'; // Role enum
 import { authorize } from '@tools/authorization'; // Page authorization
+import Table from '@components/internal/Table'; // Table component
+import Pagination from '@components/internal/Pagination'; // Pagination component
+import { SetStateAction, Dispatch } from 'react';
+import DayPicker, { DateUtils, DayPickerProps, RangeModifier } from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
+
+// Placeholder data
+
+const DATA = Array(10).fill({
+  name: {
+    name: 'Charmaine Wang',
+    id: 36565,
+  },
+  dateOfBirth: '2021/01/01',
+  homeAddress: {
+    address: '5300, No.3 Rd Lansdowne Centre',
+    city: 'Richmond',
+    postalCode: 'V2X 1E4',
+  },
+  email: 'charmainewang@rcd.org',
+  phone: '000-000-0000',
+  recentAPP: '#XXXXXX',
+  userStatus: 'active',
+});
+
+const COLUMNS = [
+  {
+    Header: 'Name',
+    accessor: 'name',
+    Cell: renderName,
+  },
+  {
+    Header: 'Date of Birth',
+    accessor: 'dateOfBirth',
+    disableSortBy: true,
+  },
+  {
+    Header: 'Home Address',
+    accessor: 'homeAddress',
+    disableSortBy: true,
+    Cell: renderAddress,
+  },
+  {
+    Header: 'Email',
+    accessor: 'email',
+    disableSortBy: true,
+  },
+  {
+    Header: 'Phone #',
+    accessor: 'phone',
+    disableSortBy: true,
+  },
+  {
+    Header: 'Recent APP',
+    accessor: 'recentAPP',
+    disableSortBy: true,
+  },
+  {
+    Header: 'User Status',
+    accessor: 'userStatus',
+    disableSortBy: true,
+    Cell: renderStatusBadge,
+  },
+];
+
+type StatusProps = {
+  value: string;
+};
+
+function renderStatusBadge({ value }: StatusProps) {
+  return (
+    <Wrap>
+      <Badge variant={value}>{value.toUpperCase()}</Badge>
+    </Wrap>
+  );
+}
+
+type AddressProps = {
+  value: any;
+};
+
+function renderAddress({ value }: AddressProps) {
+  return (
+    <>
+      <Text>{value.address}</Text>
+      <Text textStyle="caption" textColor="secondary">
+        {value.postalCode}
+      </Text>
+    </>
+  );
+}
+
+type NameProps = {
+  value: any;
+};
+
+function renderName({ value }: NameProps) {
+  return (
+    <>
+      <Text>{value.name}</Text>
+      <Text textStyle="caption" textColor="secondary">
+        {value.id}
+      </Text>
+    </>
+  );
+}
+
+type DropDownItemsProps = {
+  readonly items: Array<string>;
+  readonly setStateCallback: Dispatch<SetStateAction<string>>;
+};
+
+function DropDownItems({ items, setStateCallback }: DropDownItemsProps) {
+  return (
+    <>
+      {items.map((value, i) => (
+        <MenuItem
+          key={`item-${i}`}
+          onClick={() => {
+            setStateCallback(value);
+          }}
+        >
+          {value}
+        </MenuItem>
+      ))}
+    </>
+  );
+}
 
 // Internal home page
 export default function PermitHolders() {
+  const [permitStatusFilter, setPermitStatusFilter] = useControllableState({ defaultValue: 'All' });
+  const [userStatusFilter, setUserStatusFilter] = useControllableState({ defaultValue: 'All' });
+  const [range, setRange] = useControllableState<RangeModifier>({
+    defaultValue: { from: undefined, to: undefined },
+  });
+
+  const permitStatusOptions = [
+    'All',
+    'In Progress',
+    'Completed',
+    'Pending',
+    'Expiring Soon',
+    'Expired',
+    'Rejected',
+  ];
+  const userStatusOptions = ['All', 'Active', 'Inactive'];
+
+  const handleDayClick: DayPickerProps['onDayClick'] = day => {
+    setRange(DateUtils.addDayToRange(day, range));
+  };
+
   return (
     <Layout>
       <GridItem colSpan={12}>
-        <Text textStyle="display-xlarge">Permit Holders</Text>
+        <Flex align="left" marginBottom="32px">
+          <Text textStyle="display-xlarge">Permit Holders</Text>
+        </Flex>
+        <Box border="1px solid" borderColor="border.secondary" borderRadius="12px">
+          <Box padding="20px 24px">
+            <Flex marginBottom="20px">
+              <Menu>
+                <MenuButton
+                  as={Button}
+                  variant="outline"
+                  rightIcon={<ChevronDownIcon />}
+                  marginRight="12px"
+                  color="text.secondary"
+                  borderColor="border.secondary"
+                >
+                  Permit Status: {permitStatusFilter}
+                </MenuButton>
+                <MenuList>
+                  <DropDownItems
+                    items={permitStatusOptions}
+                    setStateCallback={setPermitStatusFilter}
+                  />
+                </MenuList>
+              </Menu>
+              <Menu>
+                <MenuButton
+                  as={Button}
+                  variant="outline"
+                  rightIcon={<ChevronDownIcon />}
+                  marginRight="12px"
+                  color="text.secondary"
+                  borderColor="border.secondary"
+                >
+                  User Status: {userStatusFilter}
+                </MenuButton>
+                <MenuList>
+                  <DropDownItems items={userStatusOptions} setStateCallback={setUserStatusFilter} />
+                </MenuList>
+              </Menu>
+              <Menu>
+                <MenuButton
+                  as={Button}
+                  variant="outline"
+                  rightIcon={<ChevronDownIcon />}
+                  marginRight="12px"
+                  color="text.secondary"
+                  borderColor="border.secondary"
+                >
+                  Expiry date:
+                  {!range.from && !range.to && ' YYYY-MM-DD - YYYY-MM-DD'}
+                  {range.from && !range.to && ' YYYY-MM-DD - YYYY-MM-DD'}
+                  {range.from &&
+                    range.to &&
+                    ` ${range.from.toLocaleDateString()} - ${range.to.toLocaleDateString()}`}
+                </MenuButton>
+                <MenuList>
+                  <DayPicker
+                    className="Selectable"
+                    numberOfMonths={2}
+                    selectedDays={[range]}
+                    onDayClick={handleDayClick}
+                  />
+                </MenuList>
+              </Menu>
+              <Box flexGrow={1}>
+                <InputGroup>
+                  <InputLeftElement pointerEvents="none">
+                    <SearchIcon color="text.filler" />
+                  </InputLeftElement>
+                  <Input placeholder="Search by first name, last name or user ID" />
+                </InputGroup>
+              </Box>
+            </Flex>
+            <Table columns={COLUMNS} data={DATA} />
+            <Flex justifyContent="flex-end">
+              <Pagination pageSize={20} totalCount={150} />
+            </Flex>
+          </Box>
+        </Box>
       </GridItem>
     </Layout>
   );
