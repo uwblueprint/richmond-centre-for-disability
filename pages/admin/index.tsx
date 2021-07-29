0fe5ffc1c110c22ea8e85a4716e8b93cac8ce3db
Angela Dietz <35577524+angeladietz@users.noreply.github.com>
2021-07-28 22:50:23 -0700

    [Feature] Build Requests Table Page (#34)
    * Added status badge, ID, dropdown filter options
    
    * Update filter dropdown text according to selection
    
    * Fix table column widths
    
    * Fix build errors
    
    * Update button text styles
    
    * pr comment changes
    
    * pr comment changes
    
    Co-authored-by: Jennifer Tsai <jennifer.tsai11@gmail.com>

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -11,16 +11,58 @@ import {
   Tab,
   Menu,
   MenuButton,
+  MenuList,
+  MenuItem,
   InputGroup,
   Input,
   InputLeftElement,
+  useControllableState,
+  Wrap,
 } from '@chakra-ui/react'; // Chakra UI
-import { AddIcon, ChevronDownIcon, SearchIcon } from '@chakra-ui/icons'; // Chakra UI Icons
+import { ChevronDownIcon, SearchIcon } from '@chakra-ui/icons'; // Chakra UI Icons
 import Layout from '@components/internal/Layout'; // Layout component
 import { Role } from '@lib/types'; // Role enum
 import { authorize } from '@tools/authorization'; // Page authorization
 import Table from '@components/internal/Table'; // Table component
 import Pagination from '@components/internal/Pagination'; // Pagination component
+import RequestStatusBadge from '@components/internal/RequestStatusBadge'; //Status badge component
+
+type StatusProps = {
+  readonly value:
+    | 'COMPLETED'
+    | 'INPROGRESS'
+    | 'PENDING'
+    | 'REJECTED'
+    | 'EXPIRING'
+    | 'EXPIRED'
+    | 'ACTIVE';
+};
+
+function renderStatusBadge({ value }: StatusProps) {
+  return (
+    <Wrap>
+      <RequestStatusBadge variant={value}></RequestStatusBadge>
+    </Wrap>
+  );
+}
+
+type NameProps = {
+  readonly value: {
+    name: string;
+    id: string;
+  };
+};
+
+function renderName({ value }: NameProps) {
+  return (
+    <div>
+      <Text>{value.name}</Text>
+      <Text textStyle="caption" textColor="secondary">
+        ID: {value.id}
+      </Text>
+    </div>
+  );
+}
 
 // Placeholder columns
 // TODO: accessors should be the accessors for these fields in the DB
@@ -28,63 +70,100 @@ const COLUMNS = [
   {
     Header: 'Name',
     accessor: 'name',
+    Cell: renderName,
+    minWidth: 240,
+    width: 280,
   },
   {
     Header: 'Date Received',
     accessor: 'dateReceived',
+    maxWidth: 240,
+    width: 240,
   },
   {
     Header: 'Permit Type',
     accessor: 'permitType',
     disableSortBy: true,
+    maxWidth: 180,
+    width: 180,
   },
   {
     Header: 'Request Type',
     accessor: 'requestType',
     disableSortBy: true,
+    maxWidth: 180,
+    width: 180,
   },
   {
     Header: 'Status',
     accessor: 'status',
     disableSortBy: true,
+    Cell: renderStatusBadge,
+    maxWidth: 180,
+    width: 100,
   },
 ];
 
 // Placeholder data
 const DATA = [
   {
-    name: 'Steve Rogers',
+    name: {
+      name: 'Steve Rogers',
+      id: '36565',
+    },
     dateReceived: 'Dec 21 2021, 8:30 pm',
     permitType: 'Permanent',
     requestType: 'Replacement',
-    status: 'In progress',
+    status: 'PENDING',
   },
   {
-    name: 'Steve Rogers',
+    name: {
+      name: 'Steve Rogers',
+      id: '36565',
+    },
     dateReceived: 'Dec 21 2021, 8:30 pm',
     permitType: 'Permanent',
     requestType: 'Replacement',
-    status: 'In progress',
+    status: 'INPROGRESS',
   },
   {
-    name: 'Steve Rogers',
+    name: {
+      name: 'Steve Rogers',
+      id: '36565',
+    },
     dateReceived: 'Dec 21 2021, 8:30 pm',
     permitType: 'Permanent',
     requestType: 'Replacement',
-    status: 'In progress',
+    status: 'COMPLETED',
   },
 ];
 
 // Internal home page - view APP requests
 export default function Requests() {
+  const [permitTypeFilter, setPermitTypeFilter] = useControllableState({ defaultValue: 'All' });
+  const [requestTypeFilter, setRequestTypeFilter] = useControllableState({ defaultValue: 'All' });
+
   return (
     <Layout>
       <GridItem colSpan={12}>
         <Flex justifyContent="space-between" alignItems="center" marginBottom="32px">
           <Text textStyle="display-xlarge">Requests</Text>
-          <Button leftIcon={<AddIcon />} height="48px" padding="24px">
-            Create a request
-          </Button>
+          {/* TODO: 'Create a new request' function is out of scope for this term's MVP
+          <Menu>
+            <MenuButton
+              as={Button}
+              leftIcon={<AddIcon width="14px" height="14px" />}
+              height="48px"
+              pl="24px"
+              pr="24px"
+            >
+              Create a request
+            </MenuButton>
+            <MenuList>
+              <MenuItem>Replacement Request</MenuItem>
+              <MenuItem>Renewal Request</MenuItem>
+            </MenuList>
+          </Menu> */}
         </Flex>
         <Box border="1px solid" borderColor="border.secondary" borderRadius="12px">
           <Tabs marginBottom="20px">
@@ -106,9 +185,39 @@ export default function Requests() {
                   marginRight="12px"
                   color="text.secondary"
                   borderColor="border.secondary"
+                  width="275px"
+                  textAlign="left"
                 >
-                  Permit type: Permanent
+                  <Text as="span" textStyle="button-semibold">
+                    Permit type:{' '}
+                  </Text>
+                  <Text as="span" textStyle="button-regular">
+                    {permitTypeFilter}
+                  </Text>
                 </MenuButton>
+                <MenuList>
+                  <MenuItem
+                    onClick={() => {
+                      setPermitTypeFilter('All');
+                    }}
+                  >
+                    All
+                  </MenuItem>
+                  <MenuItem
+                    onClick={() => {
+                      setPermitTypeFilter('Permanent');
+                    }}
+                  >
+                    Permanent
+                  </MenuItem>
+                  <MenuItem
+                    onClick={() => {
+                      setPermitTypeFilter('Temporary');
+                    }}
+                  >
+                    Temporary
+                  </MenuItem>
+                </MenuList>
               </Menu>
               <Menu>
                 <MenuButton
@@ -118,9 +227,39 @@ export default function Requests() {
                   marginRight="12px"
                   color="text.secondary"
                   borderColor="border.secondary"
+                  width="300px"
+                  textAlign="left"
                 >
-                  Request type: Replacement
+                  <Text as="span" textStyle="button-semibold">
+                    Request type:{' '}
+                  </Text>
+                  <Text as="span" textStyle="button-regular">
+                    {requestTypeFilter}
+                  </Text>
                 </MenuButton>
+                <MenuList>
+                  <MenuItem
+                    onClick={() => {
+                      setRequestTypeFilter('All');
+                    }}
+                  >
+                    All
+                  </MenuItem>
+                  <MenuItem
+                    onClick={() => {
+                      setRequestTypeFilter('Replacement');
+                    }}
+                  >
+                    Replacement
+                  </MenuItem>
+                  <MenuItem
+                    onClick={() => {
+                      setRequestTypeFilter('Renewal');
+                    }}
+                  >
+                    Renewal
+                  </MenuItem>
+                </MenuList>
               </Menu>
               <Box flexGrow={1}>
                 <InputGroup>
