c319eff41f80e7159abf3c5f587cd6a5df76268b
Oustan Ding <oustan10@gmail.com>
2021-08-27 18:00:24 -0400

    Update dependencies

--- next.config.js
+++ next.config.js
@@ -2,5 +2,6 @@ const { i18n } = require('./next-i18next.config'); // Internationalization
 
 module.exports = {
   i18n,
-  target: 'serverless',
+  // Required for Netlify build. https://github.com/vercel/next.js/issues/20487#issuecomment-753884085
+  target: 'experimental-serverless-trace',
 };
