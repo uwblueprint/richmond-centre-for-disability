3c664b64b3e1ae70e754be45bf656a6eb66a3f14
Oustan Ding <oustan10@gmail.com>
2021-08-27 17:40:46 -0400

    Add target serverless

--- next.config.js
+++ next.config.js
@@ -2,6 +2,5 @@ const { i18n } = require('./next-i18next.config'); // Internationalization
 
 module.exports = {
   i18n,
-  // Required for Netlify build. https://github.com/vercel/next.js/issues/20487#issuecomment-753884085
-  target: 'experimental-serverless-trace',
+  target: 'serverless',
 };
