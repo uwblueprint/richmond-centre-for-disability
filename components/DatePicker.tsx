17e26c0a850d8b954cac146505d01ea11e64dba1
oustan10 <oustan10@gmail.com>
2021-08-28 16:42:00 -0400

    Replace renewal form date input with DatePicker component

---
+++ components/DatePicker.tsx
@@ -0,0 +1,30 @@
+import { ChakraProps, Input } from '@chakra-ui/react'; // Chakra UI
+import ReactDayPickerInput from 'react-day-picker/DayPickerInput'; // React Day Picker Input
+import 'react-day-picker/lib/style.css'; // React Day Picker default styling
+
+// Date Picker props
+type Props = {
+  readonly value: Date; // Date value
+  readonly onDateChange: (day: Date) => void; // Callback on date change
+} & Pick<ChakraProps, 'width'>;
+
+/**
+ * Custom date picker component wrapping React Day Picker
+ * @param date The currently selected date
+ * @param onDateChange Callback on date change
+ * @returns
+ */
+export default function DatePicker(props: Props) {
+  const { width, value, onDateChange } = props;
+
+  return (
+    <>
+      <ReactDayPickerInput
+        component={(props: any) => <Input width={width} {...props} />}
+        formatDate={date => date.toLocaleDateString('en-CA')}
+        value={value}
+        onDayChange={onDateChange}
+      />
+    </>
+  );
+}
