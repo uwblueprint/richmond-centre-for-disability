725ae5a80a021265fe11af1d5258dc20ad17cbc5
Christian Chan <christian.chan325@gmail.com>
2021-10-19 23:50:46 -0400

    add todo and fixed permits in schema.prisma

--- pages/renew.tsx
+++ pages/renew.tsx
@@ -161,6 +161,7 @@ export default function Renew() {
             physicianPostalCode: doctorPostalCode,
             physicianPhone: doctorPhoneNumber,
           }),
+          //TODO: Replace with dynamic values
           usesAccessibleConvertedVan: false,
           requiresWiderParkingSpace: false,
         },
