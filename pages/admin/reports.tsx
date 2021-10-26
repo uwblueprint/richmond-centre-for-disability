34e81e16eee3fda49a2c66882ce12e41bb447300
Christian Chan <christian.chan325@gmail.com>
2021-10-25 21:46:13 -0400

    added processing date text

--- pages/admin/reports.tsx
+++ pages/admin/reports.tsx
@@ -7,6 +7,8 @@ import { authorize } from '@tools/authorization'; // Page authorization
 // import { useState } from 'react';
 import { ChevronDownIcon } from '@chakra-ui/icons'; // Chakra UI Icons
 import { Button, Box, Flex, Menu, MenuButton, MenuList } from '@chakra-ui/react'; // Chakra UI
+// import useDateRangePicker from '@tools/hooks/useDateRangePicker';
+import { DownloadIcon } from '@chakra-ui/icons'; // Chakra UI icons
 
 type MenuTextProps = {
   readonly name: string;
@@ -28,22 +30,21 @@ function MenuText({ name, value }: MenuTextProps) {
 
 // Internal home page
 export default function Reports() {
-  // const [startDate, setStartDate] = useState<Date>();
-  // const [endDate, setEndDate] = useState<Date>();
+  // const { dateRange, addDayToDateRange, dateRangeString } = useDateRangePicker();
 
   return (
     <Layout>
       <GridItem colSpan={12}>
-        <Text textStyle="display-xlarge">Reports</Text>
-      </GridItem>
-      <GridItem colSpan={12}>
+        <Flex justifyContent="space-between" alignItems="center" marginBottom="32px">
+          <Text textStyle="display-xlarge">Accountant Reports</Text>
+        </Flex>
         <Box
           border="1px solid"
           borderColor="border.secondary"
           borderRadius="12px"
           bg="background.white"
         >
-          <Box padding="24px 24px 0 24px">
+          <Box padding="24px">
             <Flex marginBottom="20px">
               <Menu>
                 <MenuButton
@@ -56,11 +57,9 @@ export default function Reports() {
                   textAlign="left"
                   width="420px"
                 >
-                  <MenuText name={`Start date`} />
+                  <MenuText name={`Start date`} value={'YYYY-MM-DD'} />
                 </MenuButton>
-                <MenuList>
-                  {/* <DateRangePicker dateRange={dateRange} onDateChange={addDayToDateRange} /> */}
-                </MenuList>
+                <MenuList>{/* <DatePicker onDateChange={addDayToDateRange} /> */}</MenuList>
               </Menu>
               <Menu>
                 <MenuButton
@@ -73,11 +72,22 @@ export default function Reports() {
                   textAlign="left"
                   width="420px"
                 >
-                  <MenuText name={`End date`} />
+                  <MenuText name={`End date`} value={'YYYY-MM-DD'} />
                 </MenuButton>
                 <MenuList></MenuList>
               </Menu>
             </Flex>
+            <Box padding="100px">
+              <Text textStyle="display-large">Processing Date:</Text>
+              <Text textStyle="display-large">01/01/2021 - 02/02/2021</Text>
+              <Flex justify="center">
+                <Text padding="16px" margin="auto" w="23em">
+                  Accounting Report has been successfully generated. Please download the report as a
+                  .csv by clicking the button below:
+                </Text>
+              </Flex>
+              <Button leftIcon={<DownloadIcon />}>Export as .CSV</Button>
+            </Box>
           </Box>
         </Box>
       </GridItem>
