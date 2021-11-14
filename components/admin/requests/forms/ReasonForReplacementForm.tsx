45aa7669718f26392eeae0c764b32998b4a94a6e
Anish Aggarwal <anish.5@hotmail.com>
2021-11-14 17:13:34 -0500

    [Feature] Build Replacement Request Frontend (#104)
    * Added Replacement Request Frontend
    
    * Fixed up changes
    
    * Fixing Changes from Design Review
    
    * Fixing changes
    
    * Remove unnecessary passHref
    
    Co-authored-by: Oustan Ding <oustan10@gmail.com>

--- components/admin/requests/forms/ReasonForReplacementForm.tsx
+++ components/admin/requests/forms/ReasonForReplacementForm.tsx
@@ -8,6 +8,7 @@ import {
   RadioGroup,
   Radio,
   Textarea,
+  SimpleGrid,
 } from '@chakra-ui/react'; // Chakra UI
 import { ReasonForReplacement as ReasonForReplacementEnum } from '@lib/graphql/types'; // Reason For Replacement Enum
 import { formatDate } from '@lib/utils/format'; // Date formatter util
@@ -30,7 +31,7 @@ export default function ReasonForReplacementForm({
 }: ReasonForReplacementProps) {
   return (
     <>
-      <FormControl as="fieldset" paddingBottom="24px">
+      <FormControl isRequired as="fieldset" paddingBottom="24px">
         <FormLabel as="legend" marginBottom="8px">
           {'Reason'}
         </FormLabel>
@@ -54,68 +55,68 @@ export default function ReasonForReplacementForm({
       {/* Conditionally render this section if Lost is selected as replacement reason */}
       {reasonForReplacement.reason === ReasonForReplacementEnum.Lost && (
         <>
-          <FormControl isRequired paddingBottom="24px">
-            <FormLabel>{`Date`}</FormLabel>
-            <Input
-              type="date"
-              value={
-                reasonForReplacement.lostTimestamp
-                  ? formatDate(new Date(reasonForReplacement.lostTimestamp), true)
-                  : ''
-              }
-              onChange={event => {
-                const updatedlostTimestamp = new Date(reasonForReplacement.lostTimestamp);
-                updatedlostTimestamp.setFullYear(parseInt(event.target.value.substring(0, 4)));
-                updatedlostTimestamp.setMonth(parseInt(event.target.value.substring(5, 7)) - 1);
-                updatedlostTimestamp.setDate(parseInt(event.target.value.substring(8, 10)));
+          <SimpleGrid columns={[1, 1, 1, 2]} spacing="32px">
+            <FormControl isRequired paddingBottom="24px">
+              <FormLabel>{`Date`}</FormLabel>
+              <Input
+                type="date"
+                value={
+                  reasonForReplacement.lostTimestamp
+                    ? formatDate(new Date(reasonForReplacement.lostTimestamp), true)
+                    : ''
+                }
+                onChange={event => {
+                  const updatedlostTimestamp = new Date(reasonForReplacement.lostTimestamp);
+                  updatedlostTimestamp.setFullYear(parseInt(event.target.value.substring(0, 4)));
+                  updatedlostTimestamp.setMonth(parseInt(event.target.value.substring(5, 7)) - 1);
+                  updatedlostTimestamp.setDate(parseInt(event.target.value.substring(8, 10)));
 
-                onChange({
-                  ...reasonForReplacement,
-                  lostTimestamp: Date.parse(updatedlostTimestamp.toString())
-                    ? updatedlostTimestamp
-                    : reasonForReplacement.lostTimestamp,
-                });
-              }}
-            />
-          </FormControl>
+                  onChange({
+                    ...reasonForReplacement,
+                    lostTimestamp: Date.parse(updatedlostTimestamp.toString())
+                      ? updatedlostTimestamp
+                      : reasonForReplacement.lostTimestamp,
+                  });
+                }}
+              />
+            </FormControl>
 
-          <FormControl paddingBottom="24px">
-            <FormLabel>
-              {'Timestamp '}
-              <Box as="span" textStyle="body-regular">
-                {'(optional)'}
-              </Box>
-            </FormLabel>
-            <Input
-              placeholder={'eg. 04:00 pm'}
-              type="time"
-              value={
-                new Date(reasonForReplacement.lostTimestamp).toLocaleTimeString('en-US', {
-                  hour12: false,
-                  hour: '2-digit',
-                  minute: '2-digit',
-                }) || ''
-              }
-              onChange={event => {
-                const updatedlostTimestamp = new Date(reasonForReplacement.lostTimestamp);
-                updatedlostTimestamp.setHours(parseInt(event.target.value.substring(0, 2)));
-                updatedlostTimestamp.setMinutes(parseInt(event.target.value.substring(3, 5)));
+            <FormControl paddingBottom="24px">
+              <FormLabel>
+                {'Timestamp '}
+                <Box as="span" textStyle="body-regular" fontSize="sm">
+                  {'(optional)'}
+                </Box>
+              </FormLabel>
+              <Input
+                type="time"
+                value={
+                  new Date(reasonForReplacement.lostTimestamp).toLocaleTimeString('en-US', {
+                    hour12: false,
+                    hour: '2-digit',
+                    minute: '2-digit',
+                  }) || ''
+                }
+                onChange={event => {
+                  const updatedlostTimestamp = new Date(reasonForReplacement.lostTimestamp);
+                  updatedlostTimestamp.setHours(parseInt(event.target.value.substring(0, 2)));
+                  updatedlostTimestamp.setMinutes(parseInt(event.target.value.substring(3, 5)));
 
-                onChange({
-                  ...reasonForReplacement,
-                  lostTimestamp: Date.parse(updatedlostTimestamp.toString())
-                    ? updatedlostTimestamp
-                    : reasonForReplacement.lostTimestamp,
-                });
-              }}
-            />
-            <FormHelperText color="text.seconday">{'hh:mm am/pm'}</FormHelperText>
-          </FormControl>
+                  onChange({
+                    ...reasonForReplacement,
+                    lostTimestamp: Date.parse(updatedlostTimestamp.toString())
+                      ? updatedlostTimestamp
+                      : reasonForReplacement.lostTimestamp,
+                  });
+                }}
+              />
+              <FormHelperText color="text.seconday">{'Example: HH:MM AM/PM'}</FormHelperText>
+            </FormControl>
+          </SimpleGrid>
 
           <FormControl isRequired paddingBottom="24px">
             <FormLabel>{'Location'}</FormLabel>
             <Input
-              placeholder={'eg. Library'}
               value={reasonForReplacement.lostLocation || ''}
               onChange={event =>
                 onChange({
@@ -129,7 +130,6 @@ export default function ReasonForReplacementForm({
           <FormControl isRequired>
             <FormLabel>{'Event description'}</FormLabel>
             <Textarea
-              placeholder={'Explain what happened.'}
               value={reasonForReplacement.description || ''}
               onChange={event =>
                 onChange({
@@ -145,54 +145,56 @@ export default function ReasonForReplacementForm({
       {/* Conditionally renders this section if Stolen is selected as replacement reason */}
       {reasonForReplacement.reason === ReasonForReplacementEnum.Stolen && (
         <>
-          <FormControl isRequired paddingBottom="24px">
-            <FormLabel>{'Police file number'}</FormLabel>
-            <Input
-              value={reasonForReplacement.stolenPoliceFileNumber || undefined}
-              onChange={event =>
-                onChange({
-                  ...reasonForReplacement,
-                  stolenPoliceFileNumber: parseInt(event.target.value),
-                })
-              }
-            />
-          </FormControl>
+          <SimpleGrid columns={[1, 1, 1, 1, 3]} spacing="26px">
+            <FormControl isRequired paddingBottom="24px">
+              <FormLabel>{'Police file number'}</FormLabel>
+              <Input
+                value={reasonForReplacement.stolenPoliceFileNumber || undefined}
+                onChange={event =>
+                  onChange({
+                    ...reasonForReplacement,
+                    stolenPoliceFileNumber: parseInt(event.target.value),
+                  })
+                }
+              />
+            </FormControl>
 
-          <FormControl paddingBottom="24px">
-            <FormLabel>
-              {'Jurisdiction '}
-              <Box as="span" textStyle="body-regular">
-                {'(optional)'}
-              </Box>
-            </FormLabel>
-            <Input
-              value={reasonForReplacement.stolenJurisdiction || ''}
-              onChange={event =>
-                onChange({
-                  ...reasonForReplacement,
-                  stolenJurisdiction: event.target.value,
-                })
-              }
-            />
-          </FormControl>
+            <FormControl paddingBottom="24px">
+              <FormLabel>
+                {'Jurisdiction '}
+                <Box as="span" textStyle="body-regular" fontSize="sm">
+                  {'(optional)'}
+                </Box>
+              </FormLabel>
+              <Input
+                value={reasonForReplacement.stolenJurisdiction || ''}
+                onChange={event =>
+                  onChange({
+                    ...reasonForReplacement,
+                    stolenJurisdiction: event.target.value,
+                  })
+                }
+              />
+            </FormControl>
 
-          <FormControl>
-            <FormLabel>
-              {'Police officer name '}
-              <Box as="span" textStyle="body-regular">
-                {'(optional)'}
-              </Box>
-            </FormLabel>
-            <Input
-              value={reasonForReplacement.stolenPoliceOfficerName || ''}
-              onChange={event =>
-                onChange({
-                  ...reasonForReplacement,
-                  stolenPoliceOfficerName: event.target.value,
-                })
-              }
-            />
-          </FormControl>
+            <FormControl>
+              <FormLabel>
+                {'Police officer name '}
+                <Box as="span" textStyle="body-regular" fontSize="sm">
+                  {'(optional)'}
+                </Box>
+              </FormLabel>
+              <Input
+                value={reasonForReplacement.stolenPoliceOfficerName || ''}
+                onChange={event =>
+                  onChange({
+                    ...reasonForReplacement,
+                    stolenPoliceOfficerName: event.target.value,
+                  })
+                }
+              />
+            </FormControl>
+          </SimpleGrid>
         </>
       )}
 
@@ -201,7 +203,6 @@ export default function ReasonForReplacementForm({
         <FormControl isRequired>
           <FormLabel>{'Event description'}</FormLabel>
           <Textarea
-            placeholder={'Explain what happened.'}
             value={reasonForReplacement.description || ''}
             onChange={event =>
               onChange({
