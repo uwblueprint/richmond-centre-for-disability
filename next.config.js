baab1e845952744b4e643387fd90006ed277811e
EmilioMena <emilio.mena128@gmail.com>
2021-12-05 16:06:41 -0500

    add shopify-sdk, create checkout on renew submit, add to storefront token to env

--- next.config.js
+++ next.config.js
@@ -2,4 +2,7 @@ const { i18n } = require('./next-i18next.config'); // Internationalization
 
 module.exports = {
   i18n,
+  env: {
+    STOREFRONT_ACCESS_TOKEN: process.env.STOREFRONT_ACCESS_TOKEN,
+  },
 };
