6d90d3ac736d4ad348016e344c4ad12de5375bbd
Christian Chan <christian.chan325@gmail.com>
2021-12-06 20:25:11 -0500

    access token is not secret

--- next.config.js
+++ next.config.js
@@ -2,7 +2,4 @@ const { i18n } = require('./next-i18next.config'); // Internationalization
 
 module.exports = {
   i18n,
-  env: {
-    STOREFRONT_ACCESS_TOKEN: process.env.STOREFRONT_ACCESS_TOKEN,
-  },
 };
