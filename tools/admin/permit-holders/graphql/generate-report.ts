e01cdd98ca937179e8b2b8a7522cff0568277286
aaggarwal10 <anish.5@hotmail.com>
2022-02-06 09:46:49 -0500

    Accountant Report API Hookup

--- tools/admin/permit-holders/graphql/generate-report.ts
+++ tools/admin/permit-holders/graphql/generate-report.ts
@@ -1,7 +1,9 @@
 import { gql } from '@apollo/client'; // GraphQL queries
 import {
   GeneratePermitHoldersReportResult,
+  GenerateAccountantReportResult,
   QueryGeneratePermitHoldersReportArgs,
+  QueryGenerateAccountantReportArgs,
 } from '@lib/graphql/types';
 
 export const GENERATE_PERMIT_HOLDERS_REPORT_QUERY = gql`
@@ -18,4 +20,20 @@ export type GeneratePermitHoldersReportRequest = QueryGeneratePermitHoldersRepor
 // Generate applicants report query result
 export type GeneratePermitHoldersReportResponse = {
   generatePermitHoldersReport: GeneratePermitHoldersReportResult;
+};
+
+export const GENERATE_ACCOUNTANT_REPORT_QUERY = gql`
+  query GenerateAccountantReportQuery($input: GenerateAccountantReportInput!) {
+    generateAccountantReport(input: $input) {
+      ok
+    }
+  }
+`;
+
+// Generate accountant report query arguments
+export type GenerateAccountantReportRequest = QueryGenerateAccountantReportArgs;
+
+// Generate accountant report query result
+export type GenerateAccountantReportResponse = {
+  generateAccountantReport: GenerateAccountantReportResult;
 };
