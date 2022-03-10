41b9042f05deeea1b37f468b93959f064ab6f57e
Jihad Bunkheila <jihadzackaria@gmail.com>
2022-03-09 21:28:04 -0500

    import prisma client into payment received handler

--- pages/api/payment-received.ts
+++ pages/api/payment-received.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 import { NextApiHandler } from 'next'; // Next
 import { Prisma, ShopifyPaymentStatus } from '@prisma/client'; // Prisma client
+import prisma from '@prisma/index'; // Prisma client
 import crypto from 'crypto'; // Verifying Shopify Request
 import getRawBody from 'raw-body';
 import sendConfirmationEmail from '@lib/reports/utils';
