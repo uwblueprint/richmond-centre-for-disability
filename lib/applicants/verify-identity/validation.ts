b23a6be26941f99e371a5157e7a4329ec4db715b
Angela Dietz <angiecdietz@gmail.com>
2022-03-20 20:41:16 -0400

    Small fix

--- lib/applicants/verify-identity/validation.ts
+++ lib/applicants/verify-identity/validation.ts
@@ -12,6 +12,6 @@ export const verifyIdentitySchema = object({
   phoneNumberSuffix: string()
     .matches(/^\d+$/, 'Must only contain numbers')
     .required('Please enter the last 4 digits of your phone number')
-    .min(3, 'Must be exactly 4 digits'),
+    .min(4, 'Must be exactly 4 digits'),
   dateOfBirth: date().required('Please enter your date of birth'),
 });
