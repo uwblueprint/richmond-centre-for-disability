6167ce4145b6096a0f1ba37113576e68f80c7850
Bonnie Chin <bonniechin.mail@gmail.com>
2022-03-23 16:13:54 -0400

    doctor's information validation yup schema

--- lib/physicians/validation.ts
+++ lib/physicians/validation.ts
@@ -34,3 +34,31 @@ export const physicianAssessmentSchema = object({
     otherwise: date().nullable().default(null),
   }),
 });
+
+
+/**
+ * Create New / Doctor's Information validation schema
+ */
+
+export const physicianSchema = object({
+  firstName: string()
+    .matches(/^[a-zA-Z]*$/, 'Must only contain letters')
+    .required('Please enter a first name'),
+  lastName: string()
+    .matches(/^[a-zA-Z]*$/, 'Must only contain letters')
+    .required('Please enter a last name'),
+  mspNumber: string()
+    .matches(/^\d+$/, 'Must only contain numbers')
+    .required('Please enter the MSP number'),
+  phoneNumber: string()
+    .min(10, 'Must be a valid phone number')
+    .required('Please enter a phone number'),
+  addressLine1: string().required('Please enter an address'),
+  addressLine2: string(),
+  city: string()
+    .matches(/^[a-zA-Z]*$/, 'Must only contain letters')
+    .required('Please enter a city'),
+  postalCode: string()
+    .length(7, 'Must be a valid postal code')
+    .required('Please enter a postal code'),
+});
