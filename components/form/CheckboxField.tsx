027c07693206c19c13f256187aef43e2705400c0
Angela Dietz <angiecdietz@gmail.com>
2022-03-24 20:42:55 -0400

    Add checkbox field

---
+++ components/form/CheckboxField.tsx
@@ -0,0 +1,29 @@
+import { FC } from 'react';
+import { useField } from 'formik';
+import { FormControl, FormErrorMessage, Text, CheckboxProps, Checkbox } from '@chakra-ui/react';
+
+type Props = CheckboxProps & {
+  readonly name: string;
+  readonly required?: boolean;
+};
+
+const CheckboxField: FC<Props> = props => {
+  const { name, required, children, ...checkboxProps } = props;
+  const [field, meta] = useField(name);
+  const isChecked = field.value;
+
+  return (
+    <FormControl isInvalid={!!meta.error && meta.touched} isRequired={required}>
+      <Checkbox {...field} isChecked={isChecked} {...checkboxProps}>
+        {children}
+      </Checkbox>
+      <FormErrorMessage>
+        <Text as="span" textStyle="body-regular">
+          {meta.touched && meta.error ? meta.error : null}
+        </Text>
+      </FormErrorMessage>
+    </FormControl>
+  );
+};
+
+export default CheckboxField;
