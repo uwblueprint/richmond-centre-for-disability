4f89a8bd99d0c1fbac3030906976840b940ed9fe
Jihad Bunkheila <jihadzackaria@gmail.com>
2022-03-29 04:22:58 -0400

    Swapped form inputs for custom formik fields

--- components/admin/requests/payment-information/Form.tsx
+++ components/admin/requests/payment-information/Form.tsx
@@ -7,22 +7,22 @@ import {
   Stack,
   FormHelperText,
   Radio,
-  RadioGroup,
   Box,
   InputGroup,
   InputLeftElement,
-  Checkbox,
-  Select,
   Grid,
   GridItem,
   Divider,
 } from '@chakra-ui/react'; // Chakra UI
-import { PaymentType, Province } from '@lib/graphql/types';
-import { ChangeEventHandler } from 'react';
+import { PaymentType } from '@lib/graphql/types';
+import TextField from '@components/form/TextField';
+import RadioGroupField from '@components/form/RadioGroupField';
+import SelectField from '@components/form/SelectField';
+import CheckboxField from '@components/form/CheckboxField';
 
 type PaymentDetailsFormProps = {
   readonly paymentInformation: PaymentInformationFormData;
-  readonly onChange: (updatedData: PaymentInformationFormData) => void;
+  readonly onChange?: (updatedData: PaymentInformationFormData) => void;
 };
 
 /**
@@ -31,68 +31,26 @@ type PaymentDetailsFormProps = {
  * @param {PaymentInformationFormData} paymentInformation Data Structure that holds all paymentInformation for a client request.
  * @param {onChangeCallback} onChange Function that uses the updated values from form.
  */
-export default function PaymentDetailsForm({
-  paymentInformation,
-  onChange,
-}: PaymentDetailsFormProps) {
-  const {
-    paymentMethod,
-    donationAmount,
-    shippingAddressSameAsHomeAddress,
-    shippingFullName,
-    shippingAddressLine1,
-    shippingAddressLine2,
-    shippingCity,
-    shippingProvince,
-    shippingCountry,
-    shippingPostalCode,
-    billingAddressSameAsHomeAddress,
-    billingFullName,
-    billingAddressLine1,
-    billingAddressLine2,
-    billingCity,
-    billingProvince,
-    billingCountry,
-    billingPostalCode,
-  } = paymentInformation;
-
-  const handleChange =
-    (field: keyof PaymentInformationFormData): ChangeEventHandler<HTMLInputElement> =>
-    event => {
-      const updatedFieldValue =
-        event.target.type === 'checkbox' ? event.target.checked : event.target.value;
-      onChange({
-        ...paymentInformation,
-        [field]: updatedFieldValue,
-      });
-    };
-
+export default function PaymentDetailsForm({ paymentInformation }: PaymentDetailsFormProps) {
   return (
     <>
       <Box paddingBottom="32px">
         <Grid templateColumns="repeat(2, 1fr)" rowGap={'24px'}>
           <GridItem rowSpan={2} colSpan={1}>
-            <FormControl as="fieldset" isRequired>
-              <FormLabel as="legend">{'Payment method'}</FormLabel>
-              <RadioGroup
-                onChange={value =>
-                  onChange({
-                    ...paymentInformation,
-                    paymentMethod: value as PaymentType,
-                  })
-                }
-                value={paymentMethod || undefined}
-              >
-                <Stack>
-                  <Radio value={'MASTERCARD' as PaymentType}>{'Mastercard'}</Radio>
-                  <Radio value={'VISA' as PaymentType}>{'Visa'}</Radio>
-                  <Radio value={'DEBIT' as PaymentType}>{'Debit'}</Radio>
-                  <Radio value={'CASH' as PaymentType}>{'Cash'}</Radio>
-                  <Radio value={'CHEQUE' as PaymentType}>{'Cheque'}</Radio>
-                  <Radio value={'E_TRANSFER' as PaymentType}>{'E-transfer'}</Radio>
-                </Stack>
-              </RadioGroup>
-            </FormControl>
+            <RadioGroupField
+              name="paymentInformation.paymentMethod"
+              label="Payment method"
+              required
+            >
+              <Stack>
+                <Radio value={'MASTERCARD' as PaymentType}>{'Mastercard'}</Radio>
+                <Radio value={'VISA' as PaymentType}>{'Visa'}</Radio>
+                <Radio value={'DEBIT' as PaymentType}>{'Debit'}</Radio>
+                <Radio value={'CASH' as PaymentType}>{'Cash'}</Radio>
+                <Radio value={'CHEQUE' as PaymentType}>{'Cheque'}</Radio>
+                <Radio value={'E_TRANSFER' as PaymentType}>{'E-transfer'}</Radio>
+              </Stack>
+            </RadioGroupField>
           </GridItem>
 
           <GridItem>
@@ -113,20 +71,18 @@ export default function PaymentDetailsForm({
           </GridItem>
 
           <GridItem colSpan={1}>
-            <FormControl>
-              <FormLabel>
-                {'Donation '}
-                <Box as="span" textStyle="body-regular" fontSize="sm">
-                  {'(optional)'}
-                </Box>
-              </FormLabel>
-              <InputGroup>
-                <InputLeftElement pointerEvents="none" color="texticon.filler" fontSize="1.2em">
-                  {'$'}
-                </InputLeftElement>
-                <Input value={donationAmount || 0} onChange={handleChange('donationAmount')} />
-              </InputGroup>
-            </FormControl>
+            <TextField
+              name="paymentInformation.donation"
+              label={
+                <>
+                  {'Donation '}
+                  <Box as="span" textStyle="body-regular" fontSize="sm">
+                    {'(optional)'}
+                  </Box>
+                </>
+              }
+              monetaryInput
+            ></TextField>
           </GridItem>
         </Grid>
       </Box>
@@ -138,84 +94,61 @@ export default function PaymentDetailsForm({
           {'Shipping Address'}
         </Text>
 
-        <Checkbox
+        <CheckboxField
+          name="paymentInformation.shippingAddressSameAsHomeAddress"
           paddingBottom="24px"
-          isChecked={shippingAddressSameAsHomeAddress}
-          onChange={handleChange('shippingAddressSameAsHomeAddress')}
         >
           {'Same as home address'}
-        </Checkbox>
+        </CheckboxField>
 
         {/* Section is hidden if same as home address checkbox is checked */}
-        {!shippingAddressSameAsHomeAddress && (
+        {!paymentInformation.shippingAddressSameAsHomeAddress && (
           <>
-            <FormControl isRequired paddingBottom="24px">
-              <FormLabel>{'Full name'}</FormLabel>
-              <Input value={shippingFullName || ''} onChange={handleChange('shippingFullName')} />
-            </FormControl>
+            <Box paddingBottom="24px">
+              <TextField name="paymentInformation.shippingFullName" label="Full name" required />
+            </Box>
 
-            <FormControl isRequired paddingBottom="24px">
-              <FormLabel>{'Address line 1'}</FormLabel>
-              <Input
-                value={shippingAddressLine1 || ''}
-                onChange={handleChange('shippingAddressLine1')}
-              />
-              <FormHelperText color="text.secondary">
-                {'Street Address, P.O. Box, Company Name, c/o'}
-              </FormHelperText>
-            </FormControl>
+            <Box paddingBottom="24px">
+              <TextField
+                name="paymentInformation.shippingAddressLine1"
+                label="Address line 1"
+                required
+              >
+                <FormHelperText color="text.secondary">
+                  {'Street Address, P.O. Box, Company Name, c/o'}
+                </FormHelperText>
+              </TextField>
+            </Box>
 
-            <FormControl paddingBottom="24px">
-              <FormLabel>
-                {'Address line 2 '}
-                <Box as="span" textStyle="body-regular" fontSize="sm">
-                  {'(optional)'}
-                </Box>
-              </FormLabel>
-              <Input
-                value={shippingAddressLine2 || ''}
-                onChange={handleChange('shippingAddressLine2')}
-              />
-              <FormHelperText color="text.secondary">
-                {'Apartment, suite, unit, building, floor, etc'}
-              </FormHelperText>
-            </FormControl>
+            <Box paddingBottom="24px">
+              <TextField name="paymentInformation.shippingAddressLine2" label="Address line 2">
+                <FormHelperText color="text.secondary">
+                  {'Apartment, suite, unit, building, floor, etc'}
+                </FormHelperText>
+              </TextField>
+            </Box>
 
-            <Stack direction="row" spacing="20px">
-              <FormControl isRequired paddingBottom="24px">
-                <FormLabel>{'City'}</FormLabel>
-                <Input value={shippingCity} onChange={handleChange('shippingCity')} />
-              </FormControl>
+            <Stack direction="row" spacing="20px" paddingBottom="24px">
+              <TextField name="paymentInformation.shippingCity" label="City" required />
 
-              <FormControl isRequired>
-                <FormLabel>{'Province / Territory'}</FormLabel>
-                <Select
-                  placeholder="Select province / territory"
-                  value={shippingProvince || undefined}
-                  onChange={event =>
-                    onChange({
-                      ...paymentInformation,
-                      shippingProvince: event.target.value as Province,
-                    })
-                  }
-                >
-                  <option value={'ON'}>Ontario</option>
-                  <option value={'BC'}>British Columbia</option>
-                </Select>
-              </FormControl>
+              <SelectField
+                name="paymentInformation.shippingProvince"
+                label="Province / Territory"
+                required
+                placeholder="Select province / territory"
+              >
+                <option value={'ON'}>Ontario</option>
+                <option value={'BC'}>British Columbia</option>
+              </SelectField>
             </Stack>
 
             <Stack direction="row" spacing="20px">
               {/* TODO: Replace with dropdown */}
-              <FormControl isRequired>
-                <FormLabel>{'Country'}</FormLabel>
-                <Input value={shippingCountry} onChange={handleChange('shippingCountry')} />
-              </FormControl>
-              <FormControl isRequired>
-                <FormLabel>{'Postal code'}</FormLabel>
-                <Input value={shippingPostalCode} onChange={handleChange('shippingPostalCode')} />
+              <TextField name="paymentInformation.shippingCountry" label="Country" required />
+
+              <TextField name="paymentInformation.shippingPostalCode" label="Postal code" required>
                 <FormHelperText color="text.secondary">{'Example: X0X 0X0'} </FormHelperText>
-              </FormControl>
+              </TextField>
             </Stack>
           </>
         )}
@@ -228,87 +161,62 @@ export default function PaymentDetailsForm({
           {'Billing Address'}
         </Text>
 
-        <Checkbox
+        <CheckboxField
+          name="paymentInformation.billingAddressSameAsHomeAddress"
           paddingBottom="24px"
-          isChecked={billingAddressSameAsHomeAddress}
-          onChange={handleChange('billingAddressSameAsHomeAddress')}
+          // isChecked={paymentInformation.billingAddressSameAsHomeAddress}
         >
           {'Same as home address'}
-        </Checkbox>
+        </CheckboxField>
 
         {/* Section is hidden if same as home address checkbox is checked */}
-        {!billingAddressSameAsHomeAddress && (
+        {!paymentInformation.billingAddressSameAsHomeAddress && (
           <>
-            <FormControl isRequired paddingBottom="24px">
-              <FormLabel>{'Full name'}</FormLabel>
-              <Input value={billingFullName || ''} onChange={handleChange('billingFullName')} />
-            </FormControl>
+            <Box paddingBottom="24px">
+              <TextField name="paymentInformation.billingFullName" label="Full name" required />
+            </Box>
 
-            <FormControl isRequired paddingBottom="24px">
-              <FormLabel>{'Address line 1'}</FormLabel>
-              <Input
-                value={billingAddressLine1 || ''}
-                onChange={handleChange('billingAddressLine1')}
-              />
-              <FormHelperText color="text.secondary">
-                {'Street Address, P.O. Box, Company Name, c/o'}
-              </FormHelperText>
-            </FormControl>
+            <Box paddingBottom="24px">
+              <TextField
+                name="paymentInformation.billingAddressLine1"
+                label="Address line 1"
+                required
+              >
+                <FormHelperText color="text.secondary">
+                  {'Street Address, P.O. Box, Company Name, c/o'}
+                </FormHelperText>
+              </TextField>
+            </Box>
 
-            <FormControl paddingBottom="24px">
-              <FormLabel>
-                {'Address line 2 '}
-                <Box as="span" textStyle="body-regular" fontSize="sm">
-                  {'(optional)'}
-                </Box>
-              </FormLabel>
-              <Input
-                value={billingAddressLine2 || ''}
-                onChange={handleChange('billingAddressLine2')}
-              />
-              <FormHelperText color="text.secondary">
-                {'Apartment, suite, unit, building, floor, etc'}
-              </FormHelperText>
-            </FormControl>
+            <Box paddingBottom="24px">
+              <TextField name="paymentInformation.billingAddressLine2" label="Address line 2">
+                <FormHelperText color="text.secondary">
+                  {'Apartment, suite, unit, building, floor, etc'}
+                </FormHelperText>
+              </TextField>
+            </Box>
 
             <Stack direction="row" spacing="20px" paddingBottom="24px">
-              <FormControl isRequired>
-                <FormLabel>{'City'}</FormLabel>
-                <Input value={billingCity} onChange={handleChange('billingCity')} />
-              </FormControl>
+              <TextField name="paymentInformation.billingCity" label="City" required />
 
-              <FormControl>
-                <FormLabel>{'Province / Territory'}</FormLabel>
-                <Select
-                  placeholder="Select province / territory"
-                  value={billingProvince || undefined}
-                  onChange={event =>
-                    onChange({
-                      ...paymentInformation,
-                      billingProvince: event.target.value as Province,
-                    })
-                  }
-                >
-                  <option value={'ON'}>Ontario</option>
-                  <option value={'BC'}>British Columbia</option>
-                </Select>
-              </FormControl>
+              <SelectField
+                name="paymentInformation.billingProvince"
+                label="Province / Territory"
+                placeholder="Select province / territory"
+                required
+              >
+                <option value={'ON'}>Ontario</option>
+                <option value={'BC'}>British Columbia</option>
+              </SelectField>
             </Stack>
 
             <Stack direction="row" spacing="20px">
               {/* TODO: Replace with dropdown */}
-              <FormControl isRequired>
-                <FormLabel>{'Country'}</FormLabel>
-                <Input value={billingCountry} onChange={handleChange('billingCountry')} />
-              </FormControl>
-              <FormControl isRequired>
-                <FormLabel>{'Postal code'}</FormLabel>
-                <Input
-                  value={billingPostalCode || ''}
-                  onChange={handleChange('billingPostalCode')}
-                />
+              <TextField name="paymentInformation.billingCountry" label="Country" required />
+
+              <TextField name="paymentInformation.billingPostalCode" label="Country" required>
                 <FormHelperText color="text.secondary">{'Example: X0X 0X0'} </FormHelperText>
-              </FormControl>
+              </TextField>
             </Stack>
           </>
         )}
--- components/form/TextField.tsx
+++ components/form/TextField.tsx
@@ -7,22 +7,38 @@ import {
   Text,
   Input,
   InputProps,
+  InputGroup,
+  InputLeftElement,
 } from '@chakra-ui/react';
 
 type Props = InputProps & {
   readonly name: string;
   readonly label: string | ReactNode;
   readonly required?: boolean;
+  readonly monetaryInput?: boolean;
 };
 
 const TextField: FC<Props> = props => {
-  const { name, label, required, children, ...inputProps } = props;
+  const { name, label, required, children, monetaryInput, ...inputProps } = props;
   const [field, meta] = useField(name);
 
   return (
     <FormControl isInvalid={!!meta.error && meta.touched} isRequired={required}>
       <FormLabel htmlFor={name}>{label}</FormLabel>
-      <Input {...field} {...inputProps} />
+      {monetaryInput ? (
+        <>
+          <InputGroup>
+            <InputLeftElement pointerEvents="none" color="texticon.filler" fontSize="1.2em">
+              {'$'}
+            </InputLeftElement>
+            <Input {...field} {...inputProps} />
+          </InputGroup>
+        </>
+      ) : (
+        <>
+          <Input {...field} {...inputProps} />
+        </>
+      )}
       <FormErrorMessage>
         <Text as="span" textStyle="body-regular">
           {meta.touched && meta.error ? meta.error : null}
