3d4f78a7ef81faf05298879d60e1c0ba22f09874
Christian Chan <christian.chan325@gmail.com>
2022-04-27 19:10:24 -0400

    upload permit holder report csv with auto download

--- lib/graphql/types.ts
+++ lib/graphql/types.ts
@@ -488,6 +488,7 @@ export type GeneratePermitHoldersReportInput = {
 export type GeneratePermitHoldersReportResult = {
   __typename?: 'GeneratePermitHoldersReportResult';
   ok: Scalars['Boolean'];
+  url: Maybe<Scalars['String']>;
 };
 
 export type Guardian = {
