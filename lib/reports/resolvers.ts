7a112ca1fa2c7cb8944e10f68bf1dc99462922ec
Jihad Bunkheila <51224641+j-bunk@users.noreply.github.com>
2022-05-01 13:34:34 -0400

    [Feature] Upload accounting report to S3 (#176)
    * Upload csv and auto download
    
    * Fix file naming
    
    * Change naming format comment
    
    Co-authored-by: oustan10 <oustan10@gmail.com>

--- lib/reports/resolvers.ts
+++ lib/reports/resolvers.ts
@@ -308,15 +308,20 @@ export const generateApplicationsReport: Resolver<
 
 /**
  * Generates csv with accountants' info, given a start date and end date
- * @returns Whether a csv could be generated (ok), and in the future an AWS S3 file link
+ * @returns Whether a csv could be generated (ok), and an AWS S3 file link
  */
 export const generateAccountantReport: Resolver<
   QueryGenerateAccountantReportArgs,
   GenerateAccountantReportResult
-> = async (_, args, { prisma }) => {
+> = async (_, args, { prisma, session }) => {
   const {
     input: { startDate, endDate },
   } = args;
+
+  if (!session) {
+    // TODO: Create error
+    throw new ApolloError('Not authenticated');
+  }
 
   const paymentTypeToString: Record<PaymentType, string> = {
     MASTERCARD: 'Mastercard (Office)',
@@ -392,13 +397,35 @@ export const generateAccountantReport: Resolver<
     { id: 'donationAmount', title: 'Donation' },
     { id: 'totalAmount', title: 'Total' },
   ];
-  const csvWriter = createObjectCsvWriter({
-    path: 'temp/file.csv',
+
+  // Generate CSV string from csv object.
+  const csvStringifier = createObjectCsvStringifier({
     header: csvHeaders,
   });
-  await csvWriter.writeRecords(csvAccountantReportRows);
+  const csvStringRecords = csvStringifier.stringifyRecords(csvAccountantReportRows);
+  const csvStringHeader = csvStringifier.getHeaderString();
+  const csvString = csvStringHeader + csvStringRecords;
+
+  // CSV naming format accounting-report-{employeeID}-{timestamp}.csv
+  const employeeID = session.id;
+  const timestamp = formatDateTimeYYYYMMDDHHMMSS(new Date());
+  const fileName = `accounting-report-${employeeID}-${timestamp}.csv`;
+  const s3ObjectKey = `rcd/reports/${fileName}`;
+
+  // Upload csv to s3
+  let uploadedCSV;
+  let signedUrl;
+  try {
+    // Upload file to s3
+    uploadedCSV = await serverUploadToS3(csvString, s3ObjectKey);
+    // Generate a signed URL to access the file
+    signedUrl = getSignedUrlForS3(uploadedCSV.key, 10, true);
+  } catch (error) {
+    throw new ApolloError(`Error uploading accountant report to AWS: ${error}`);
+  }
 
   return {
     ok: true,
+    url: signedUrl,
   };
 };
