cb343912f51b1e07b2656f9a18875664d206f723
Jihad Bunkheila <51224641+j-bunk@users.noreply.github.com>
2022-05-08 00:16:05 -0400

    [Feature] Create Physician Assessment Card (#184)
    * Built card base + gql query
    
    * Fixed styling + addressed NOTETOSELF comments
    
    * Picked response fields

--- tools/admin/requests/physician-assessment.ts
+++ tools/admin/requests/physician-assessment.ts
@@ -1,6 +1,10 @@
-import { PermitType } from '@lib/graphql/types';
-import { PatientCondition } from '@lib/graphql/types';
-import { NewApplication } from '@lib/graphql/types';
+import {
+  NewApplication,
+  PermitType,
+  PatientCondition,
+  QueryApplicationArgs,
+} from '@lib/graphql/types';
+import { gql } from '@apollo/client';
 
 /** Physician assessment in forms */
 export type PhysicianAssessment = Pick<
@@ -9,4 +13,36 @@ export type PhysicianAssessment = Pick<
 > & {
   patientCondition: PatientCondition | null;
   permitType: PermitType | null;
+};
+
+/** Get physician assessment information of an application */
+export const GET_PHYSICIAN_ASSESSMENT = gql`
+  query GetPhysicianAssessment($id: Int!) {
+    application(id: $id) {
+      id
+      __typename
+      ... on NewApplication {
+        disability
+        disabilityCertificationDate
+        otherPatientCondition
+        temporaryPermitExpiry
+        patientCondition
+        permitType
+      }
+    }
+  }
+`;
+
+export type GetPhysicianAssessmentRequest = QueryApplicationArgs;
+
+export type GetPhysicianAssessmentResponse = {
+  application: Pick<
+    NewApplication,
+    | 'patientCondition'
+    | 'permitType'
+    | 'disability'
+    | 'disabilityCertificationDate'
+    | 'otherPatientCondition'
+    | 'temporaryPermitExpiry'
+  >;
 };
