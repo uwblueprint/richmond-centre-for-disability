37b9ae09052297aab9c0a88e9b2133cb5566466b
Oustan Ding <oustan10@gmail.com>
2022-08-07 04:28:41 -0400

    [Fix] Remove GQL playground redirect (#245)

--- next.config.js
+++ next.config.js
@@ -2,18 +2,4 @@ const { i18n } = require('./next-i18next.config'); // Internationalization
 
 module.exports = {
   i18n,
-  async redirects() {
-    const redirects = [];
-
-    if (process.env.NODE_ENV !== 'development') {
-      // Disable GraphQL playground in production
-      redirects.push({
-        source: '/api/graphql',
-        destination: '/',
-        permanent: true,
-      });
-    }
-
-    return redirects;
-  },
 };
