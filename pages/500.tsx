a8a931cffba5e213ec52c7374934f68454d7b915
Oustan Ding <oustan10@gmail.com>
2022-08-08 23:26:30 -0400

    [Improvement] Various fixes (#246)

---
+++ pages/500.tsx
@@ -0,0 +1,25 @@
+import { GridItem, Text, VStack } from '@chakra-ui/react';
+import Layout from '@components/applicant/Layout';
+import { NextPage } from 'next';
+import Link from 'next/link';
+
+const ErrorPage: NextPage = () => {
+  return (
+    <Layout>
+      <GridItem colSpan={12}>
+        <VStack align="stretch" spacing="24px" minHeight="calc(100vh - 280px)">
+          <Text as="h1" textStyle="display-xlarge">
+            500 Internal error
+          </Text>
+          <Link href="/" passHref>
+            <Text as="a" textStyle="button-regular" color="primary">
+              Back to home
+            </Text>
+          </Link>
+        </VStack>
+      </GridItem>
+    </Layout>
+  );
+};
+
+export default ErrorPage;
