118ef98b71791c54615db58a00b52280192b7b54
Oustan Ding <oustan10@gmail.com>
2022-09-01 23:27:18 -0400

    Fix TS build error

--- tools/admin/permit-holders/app-history.ts
+++ tools/admin/permit-holders/app-history.ts
@@ -2,7 +2,7 @@ import { Application, ApplicationProcessing, Invoice, Permit } from '@lib/graphq
 
 /** APP history entry record (API response) */
 export type AppHistoryRecord = Pick<Permit, 'rcdPermitId' | 'expiryDate'> & {
-  application: Pick<Application, 'id' | 'permitType'> & {
+  application: Pick<Application, 'id' | 'type' | 'permitType'> & {
     processing: Pick<ApplicationProcessing, 'documentsUrl' | 'documentsS3ObjectKey'> & {
       invoice: Pick<Invoice, 's3ObjectUrl' | 's3ObjectKey'>;
     };
