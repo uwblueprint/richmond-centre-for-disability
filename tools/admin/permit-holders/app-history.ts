9277461eed69448042aef0f79c34bd4c19d13820
Oustan Ding <oustan10@gmail.com>
2022-09-01 23:59:48 -0400

    [Fix] Fix APP history permit type badge (#273)
    * Query permitType field for all application types
    
    * Fix TS build error

--- tools/admin/permit-holders/app-history.ts
+++ tools/admin/permit-holders/app-history.ts
@@ -1,30 +1,19 @@
-import {
-  Application,
-  ApplicationProcessing,
-  Invoice,
-  NewApplication,
-  Permit,
-  PermitType,
-} from '@lib/graphql/types';
+import { Application, ApplicationProcessing, Invoice, Permit } from '@lib/graphql/types';
 
 /** APP history entry record (API response) */
 export type AppHistoryRecord = Pick<Permit, 'rcdPermitId' | 'expiryDate'> & {
-  application: Pick<Application, 'id'> & {
+  application: Pick<Application, 'id' | 'type' | 'permitType'> & {
     processing: Pick<ApplicationProcessing, 'documentsUrl' | 'documentsS3ObjectKey'> & {
       invoice: Pick<Invoice, 's3ObjectUrl' | 's3ObjectKey'>;
     };
-  } & (
-      | ({ type: 'NEW' } & Pick<NewApplication, 'permitType'>)
-      | { type: 'RENEWAL' | 'REPLACEMENT'; permitType: undefined }
-    );
+  };
 };
 
 /** APP history entry row in APP history card (FE) */
 export type PermitRecord = Pick<Permit, 'rcdPermitId' | 'expiryDate'> & {
-  application: Pick<Application, 'id' | 'type'> & {
+  application: Pick<Application, 'id' | 'type' | 'permitType'> & {
     processing: Pick<ApplicationProcessing, 'documentsUrl' | 'documentsS3ObjectKey'> & {
       invoice: Pick<Invoice, 's3ObjectUrl' | 's3ObjectKey'>;
     };
-    permitType: PermitType | undefined;
   };
 };
