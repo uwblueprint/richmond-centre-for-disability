277cb513617a99f4780247317a4364fdd71043f4
ChinemeremChigbo <chinemeremchigbo@Outlook.com>
2023-10-31 14:35:38 +0800

    Add defaults for lost or stolen

--- components/admin/requests/reason-for-replacement/Form.tsx
+++ components/admin/requests/reason-for-replacement/Form.tsx
@@ -36,8 +36,22 @@ export default function ReasonForReplacementForm({
       <Box paddingBottom="24px">
         <RadioGroupField name="reasonForReplacement.reason" label="Reason" required>
           <Stack>
-            <Radio value={'LOST'}>{'Lost'}</Radio>
-            <Radio value={'STOLEN'}>{'Stolen'}</Radio>
+            <Radio
+              value={'LOST'}
+              onChange={() => {
+                setFieldValue('paymentInformation.processingFee', '31');
+              }}
+            >
+              {'Lost'}
+            </Radio>
+            <Radio
+              value={'STOLEN'}
+              onChange={() => {
+                setFieldValue('paymentInformation.processingFee', '31');
+              }}
+            >
+              {'Stolen'}
+            </Radio>
             <Radio
               value={'OTHER'}
               onChange={() => {
