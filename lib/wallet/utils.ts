65605f0328e8f5511c048c70907b7580e85f5c14
Sharujan Sreekaran <ohnose123@gmail.com>
2024-10-20 20:07:16 -0400

    Update resolvers to upload test pdf to s3 bucket
    (cherry picked from commit dc1b7c18eeec4ca3edf0e977048d781125f12f3b)

---
+++ lib/wallet/utils.ts
@@ -0,0 +1,40 @@
+import logger from '@lib/utils/logging';
+import PdfPrinter from 'pdfmake';
+
+const walletPdfDefinition = () => {
+  const PTS_TO_INCH = 72;
+  return {
+    content: [
+      {
+        image: 'cardFront',
+        width: 3.5 * PTS_TO_INCH,
+        height: 2 * PTS_TO_INCH,
+        absolutePosition: { x: 100, y: 100 },
+      },
+    ],
+    images: {
+      cardFront: 'public/assets/card-front.png',
+    },
+  };
+};
+
+export const generateWalletCard = (): PDFKit.PDFDocument | null => {
+  let pdfDoc = null;
+  try {
+    const printer = new PdfPrinter({
+      Helvetica: {
+        normal: 'Helvetica',
+        bold: 'Helvetica-Bold',
+        italics: 'Helvetica-Oblique',
+        bolditalics: 'Helvetica-BoldOblique',
+      },
+    });
+    const documentDef = walletPdfDefinition();
+    pdfDoc = printer.createPdfKitDocument(documentDef);
+    pdfDoc.end();
+  } catch (err) {
+    logger.error({ error: err }, 'Error Generating Wallet PDF: ', err);
+    return null;
+  }
+  return pdfDoc;
+};
